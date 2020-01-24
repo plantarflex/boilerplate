4fc867e50eac7d588fc70a4713cd63eba593ba54
plantarflex <plantarflex@naver.com>
2020-01-25 03:56:44 +0900

    fix minor typo

--- viki/logger.py
+++ viki/logger.py
@@ -2,18 +2,21 @@ import pathlib
 import logging
 import logging.handlers
 
+from config import Config
 
 class Logger:
     def __init__(self):
         logging.basicConfig(level=logging.NOTSET)
-        self._format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        self._format = logging.Formatter('%(asctime)s - [Thread %(name)s] - %(levelname)s - %(message)s')
         self._loggers = {}
 
     def create(self, thread_name, levels=None):
         pathlib.Path('./logs/{}'.format(thread_name)).mkdir(parents=True, exist_ok=True)
-        logger_name = '[Thread {}]'.format(thread_name)
-        if self._loggers.get(logger_name) is None:
-            self._loggers[logger_name] = logging.getLogger(logger_name)
+        if type(thread_name) is not str:
+            print('thread name should be string type')
+            raise ValueError
+        if self._loggers.get(thread_name) is None:
+            self._loggers[thread_name] = logging.getLogger(thread_name)
             self._attach_handlers(thread_name, levels)
 
     def __getitem__(self, thread_name):
@@ -30,27 +33,27 @@ class Logger:
             }
         if levels is None or len(levels) == 0:
             levels = ['DEBUG']
-        for level in levels
-            self.loggers[thread_name].addHandler(handlers[level])
+        for level in levels:
+            self._loggers[thread_name].addHandler(handlers[level](thread_name))
 
     def _debug_handler(self, thread_name):
-        handler = logging.Streamhandler()
-        handler.setformatter(self._format)
-        handler.setlevel(logging.DEBUG)
+        handler = logging.StreamHandler()
+        handler.setFormatter(self._format)
+        handler.setLevel(logging.DEBUG)
         return handler
 
     def _info_handler(self, thread_name):
-        handler = logging.handlers.RotatingFilehandler(
-                './logs/{}/info.log'.format(thread_name),
-                maxbytes=basicconfig.log_file_size
-            )
-        handler.setformatter(self._format)
-        handler.setlevel(logging.INFO)
-        return handler
+       handler = logging.handlers.RotatingFileHandler(
+           './logs/{}/info.log'.format(thread_name),
+           maxBytes=Config.LOG_FILE_SIZE
+           )
+       handler.setFormatter(self._format)
+       handler.setLevel(logging.INFO)
+       return handler
 
     def _error_handler(self, thread_name):
         handler = logging.FileHandler('./logs/{}/error.log'.format(thread_name))
-        handler.setformatter(self._format)
+        handler.setFormatter(self._format)
         handler.setLevel(logging.ERROR)
         return handler
 
